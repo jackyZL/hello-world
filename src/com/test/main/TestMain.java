7eb32c9b110ed1cea50397c569a989842309d943
张龙 <zhangl@xiaopeng.com>
2016-09-19 10:13:37 +0800

    Initial commit

---
+++ src/com/test/main/TestMain.java
@@ -0,0 +1,10 @@
+package com.test.main;
+
+/**
+ * Created by jacky on 2016/9/19.
+ */
+public class TestMain {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
